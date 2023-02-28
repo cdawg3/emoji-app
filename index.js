d9666fc46024d8ec59721e2bef5172cb12b1c796
cdawg3 <cdawg4604@gmail.com>
2023-02-28 10:54:39 -0600

    added styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"];
+const myEmojis = ["👨‍💻", "🎮", "🏋️‍♂️"];
 const emojiContainer = document.getElementById("emojiContainer");
 const emojiInput = document.getElementById("emoji-input");
 const pushBtn = document.getElementById("push-btn");
